e1cf2399e275ba73d8de0eaee690d7a89eaadc71
Simon Sebright <simonsebright@hotmail.com>
2017-12-12 14:34:58 +0100

    Starting point

---
+++ SOLIDTrainingLetterO/LetterORight/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LetterORight
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            try
+            {
+                // Call your code here!
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
