45f0677c76806bba48efa162783951f2bb21fa00
Simon Sebright <simonsebright@hotmail.com>
2017-12-13 00:00:31 +0100

    Test for line-based reading of email addresses from stream

--- SOLIDTrainingLetterO/RightTest/MailingLogicTest.cs
+++ SOLIDTrainingLetterO/RightTest/MailingLogicTest.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using FluentAssertions;
 using Xunit;
 
@@ -8,10 +10,24 @@ namespace RightTest
 {
     public class MailingLogicTest
     {
+
         [Fact]
-        public void Email_address_is_validated()
+        public void Target_called_for_valid_addresses()
         {
-            var emailAddresses = new string[] { "a@b.com", "rubbish" };
+            var emailAddresses = new string[] {"a@b.com", "b@c.com"};
+            int count = 0;
+
+            MailingLogic.Process(emailAddresses,
+                (em) => true,
+                (em) => { count++; });
+
+            count.Should().Be(2);
+        }
+
+        [Fact]
+        public void Target_not_called_for_invalid_addresses()
+        {
+            var emailAddresses = new string[] {"a@b.com", "rubbish"};
             int count = 0;
 
             MailingLogic.Process(emailAddresses,
@@ -22,19 +38,28 @@ namespace RightTest
         }
 
         [Fact]
-        public void Target_called_for_valid_addresses()
+        public void Real_validator_checks_for_whitespace_and_at()
         {
-            var emailAddresses = new string[] { "a@b.com", "b@c.com" };
-            int count = 0;
+            MailingLogic.IsValidEmailAddress(null).Should().BeFalse();
+            MailingLogic.IsValidEmailAddress(String.Empty).Should().BeFalse();
+            MailingLogic.IsValidEmailAddress("\t\r\n").Should().BeFalse();
+            MailingLogic.IsValidEmailAddress("This has no at sign, is not email").Should().BeFalse();
 
-            MailingLogic.Process(emailAddresses,
-                (em) => true,
-                (em) => { count++; });
+            MailingLogic.IsValidEmailAddress("@").Should().BeTrue();
+        }
 
-            count.Should().Be(2);
+        [Fact]
+        public void Line_based_address_reader()
+        {
+            var contents = 
+@"a@b.com
+b@c.com";
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(contents)))
+            {
+                MailingLogic.LineBasedEmailAddresses(ms).ShouldBeEquivalentTo(new string[] {"a@b.com", "b@c.com"});
+            }
         }
     }
-
 
     public static class MailingLogic
     {
@@ -45,6 +70,23 @@ namespace RightTest
             foreach (var validAddress in validAddresses)
             {
                 target(validAddress);
+            }
+        }
+
+        public static bool IsValidEmailAddress(string email)
+        {
+            // Obviously this would be a bit better....
+            return !String.IsNullOrWhiteSpace(email) && email.Contains("@");
+        }
+
+        public static IEnumerable<string> LineBasedEmailAddresses(Stream contents)
+        {
+            using (StreamReader sr = new StreamReader(contents))
+            {
+                while (!sr.EndOfStream)
+                {
+                    yield return sr.ReadLine();
+                }
             }
         }
     }
