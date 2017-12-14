9f06bd35dcd1de1ad8d6b3be558e82d4f30e657e
SimonSebright <simon.sebright@isolutions.ch>
2017-12-14 11:54:45 +0100

    Next lesson, L, starting point

---
+++ SOLIDTrainingLetterL/LetterLRight/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LetterLRight
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("1 = Rectangle, 2 = Square");
+
+            var userDecision = Console.ReadLine();
+
+            // Write your code here!
+
+            Console.ReadLine();
+        }
+
+        private static void ValidateLiskov(Figure figure, int expectedArea)
+        {
+            var area = figure.CalculateArea();
+            Console.WriteLine($"Area is {area} - expected {expectedArea}");
+            var foregroundColor = Console.ForegroundColor;
+            try
+            {
+                if (area != expectedArea)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Liskov violated because Height should not have been modified.");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Liskov principal handled correctly.");
+                }
+            }
+            finally
+            {
+                Console.ForegroundColor = foregroundColor;
+            }
+        }
+    }
+}
