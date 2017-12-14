3cf72e802794675c0c0e3e895bc956ac5922c0f4
SimonSebright <simon.sebright@isolutions.ch>
2017-12-14 12:27:54 +0100

    Added Circle

---
+++ SOLIDTrainingLetterL/LetterLRight/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterLRight
+{
+    public class Circle : Figure
+    {
+        private int _radius;
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object"></see> class.</summary>
+        public Circle(int radius)
+        {
+            Console.WriteLine($"Creating {typeof(Circle).FullName} with radius of {radius}");
+            _radius = radius;
+        }
+
+        public int Radius
+        {
+            get => _radius;
+            set
+            {
+                Console.WriteLine($"Updating {nameof(Radius)} to {value}.");
+                _radius = value;
+            }
+        }
+
+
+        public override int CalculateArea()
+        {
+            return (int) Math.Floor(3.142 * _radius * _radius);
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine($"Circle draw with radius  {_radius}");
+        }
+    }
+}
