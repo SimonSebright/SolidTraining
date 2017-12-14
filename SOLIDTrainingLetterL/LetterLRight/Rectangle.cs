ee8a2cd71dcc18ebab20292e2028d357d45150b9
SimonSebright <simon.sebright@isolutions.ch>
2017-12-14 12:13:03 +0100

    Make both classes directly come from figure

---
+++ SOLIDTrainingLetterL/LetterLRight/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterLRight
+{
+    public class Rectangle : Figure
+    {
+        private int _height;
+        private int _width;
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object"></see> class.</summary>
+        public Rectangle(int width, int height)
+        {
+            Console.WriteLine($"Creating {typeof(Rectangle).FullName} with width of {width} and height of {height}");
+
+            Width = width;
+            Height = height;
+        }
+
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                Console.WriteLine($"Updating {nameof(Width)} to {value}.");
+                _width = value;
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                Console.WriteLine($"Updating {nameof(Height)} to {value}.");
+                _height = value;
+            }
+        }
+
+        public override int CalculateArea()
+        {
+            return _height * _width;
+        }
+    }
+}
---
+++ SOLIDTrainingLetterL/LetterLRight/Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetterLRight
+{
+    public class Square : Figure
+    {
+        private int _sideLength;
+
+        /// <summary>Initializes a new instance of the <see cref="T:System.Object"></see> class.</summary>
+        public Square(int sideLength)
+        {
+            Console.WriteLine($"Creating {typeof(Square).FullName} with sideLength of {sideLength}");
+            _sideLength = sideLength;
+        }
+
+        public int SideLength
+        {
+            get => _sideLength;
+            set => _sideLength = value;
+        }
+        public int Width
+        {
+            get => _sideLength;
+        }
+
+        public int Height
+        {
+            get => _sideLength;
+        }
+
+        public override int CalculateArea()
+        {
+            return _sideLength * _sideLength;
+        }
+    }
+}
